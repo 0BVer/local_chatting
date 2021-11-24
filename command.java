935104104bfb7f227a9f994d5199123cf51e7d73
Young Jun Kim <73998876+0BVer@users.noreply.github.com>
2021-11-24 20:56:18 +0900

    active successful!

--- command.java
+++ command.java
@@ -3,7 +3,6 @@ package chat;
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 public class command implements Serializable{
     int command_type;
